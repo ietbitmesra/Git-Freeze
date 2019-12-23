6953a3e39e7bd2351a48494fb111cefb816e7af9
aryan29 <nk28agra@gmail.com>
2019-12-23 23:56:58 +0530

    Api.py Improved and checked

--- Week-1/aryan29/api.py
+++ Week-1/aryan29/api.py
@@ -3,108 +3,136 @@ import json
 import requests
 import bs4
 from termcolor import cprint
-with open("user_config.json","r") as f:
-    conf=json.load(f)
-    if(len(conf["mob"])==0):
-        cprint("Please Configure your user_config.json file and add Code of Atleast 1 mobile Phone to generate json",'red')
+with open("user_config.json", "r") as f:
+    CONF = json.load(f)
+    if (len(CONF["mob"]) == 0):
+        cprint(('Please Configure your user_config.json file and add'
+                'Code of Atleast 1 mobile Phone to generate json'), 'red')
         sys.exit()
+
+
 class Api:
-    def get_data(self,mob):
-        b=requests.get(f"https://www.smartprix.com/mobiles/{mob[0]}")
-        l1=bs4.BeautifulSoup(b.text,'lxml')
-        price2=l1.select("#product-price .price")[0]
+    def get_data(self, mob):
+        req1 = requests.get(f"https://www.smartprix.com/mobiles/{mob[0]}")
+        soup1 = bs4.BeautifulSoup(req1.text, 'lxml')
+        price1 = soup1.select("#product-price .price")[0]
         try:
-            a=requests.get(f"https://www.mysmartprice.com/mobile/{mob[1]}")
-            l=bs4.BeautifulSoup(a.text,'lxml')
-            price1=l.select(".prdct-dtl__prc-val")[0]
-            price=self.getminprice(price1,price2)
-        except:
-            price=price2
-        a=l1.select(".has-favorite-button , .cons span , .pros span")
-        f_list=a
-        f_list.append(price)
-        return f_list
-    def getminprice(self,price1,price2):
-        #print((price1.getText()),price2.getText()[1:])
-        if((price1.getText(),10)<(price2.getText()[1:],10)):
+            req2 = requests.get(
+                f"https://www.mysmartprice.com/mobile/{mob[1]}"
+                )
+            soup2 = bs4.BeautifulSoup(req2.text, 'lxml')
+            price2 = soup2.select(".prdct-dtl__prc-val")[0]
+            price = self.getminprice(price2, price1)
+        except Exception as ex:
+            price = price1
+        myList = soup1.select(".has-favorite-button , .cons span , .pros span")
+        myList.append(price)
+        return myList
+
+    def getminprice(self, price1, price2):
+        # print((price1.getText()),price2.getText()[1:])
+        if ((price1.getText(), 10) < (price2.getText()[1:], 10)):
             return price1
         else:
             return price2
-    def name_data(self,mob,code):
-        list=[]
-        for i in range(len(mob)):
-            list.append(mob[i].getText().replace('\u2009',' ').replace('\u20b9',''))
-        #print(list)
-        dict={"name":list[0],"RAM/Internal Storage":list[3],"Battery":list[4],"Camera":list[6],"Screen-Size":list[5],"Connectivity":list[1],"OS":list[8],"Best Price":list[9],"Processor":list[2],"Code":code}
-        return dict
-    def generate_json(self,mob):
-        list=[]
+
+    def name_data(self, mob, code):
+        myList = []
+        for spec in mob:
+            myList.append(spec.getText()
+                          .replace('\u2009', ' ').replace('\u20b9', ''))
+
+        # print(List)
+        myDict = {"name": myList[0],
+                  "RAM/Internal Storage": myList[3],
+                  "Battery": myList[4],
+                  "Camera": myList[6],
+                  "Screen-Size": myList[5],
+                  "Connectivity": myList[1],
+                  "OS": myList[8],
+                  "Best Price": myList[9],
+                  "Processor": myList[2],
+                  "Code": code}
+        return myDict
+
+    def generate_json(self, mob):
         try:
-            f=open("data.json","r")
-            data=json.load(f)
+            f = open("data.json", "r")
+            data = json.load(f)
             f.close()
-        except:
-            f=open("data.json","w")
-            print("[]",file=f)
-            data=[]
+        except Exception as ex:
+            f = open("data.json", "w")
+            print("[]", file=f)
+            data = []
             f.close()
-        with open("data.json","w") as f:
+        with open("data.json", "w") as f:
             data.append(mob)
-            json.dump(data,f,indent=4)
+            json.dump(data, f, indent=4)
+
     def organize(self):
-        for i in conf["mob"]:
-            mob=conf["mob"][i]
-            m=self.get_data(mob)
-            j=self.name_data(m,mob)
+        for i in CONF["mob"]:
+            mob = CONF["mob"][i]
+            m = self.get_data(mob)
+            j = self.name_data(m, mob)
             self.generate_json(j)
-        cprint("Json File successfull generated Do you want to Receive Price Updates on this email?(y/n)",'yellow')
-        res=input()
-        if(res=="Y" or res=="y"):
-            email=input("Enter your Email")
-            conf['receiver_email'].append(email)
-            data=conf
-            with open("user_config.json","w") as f:
-                json.dump(data,f,indent=4)
-                cprint("We added your email successfully",'green')
+        cprint(("Json File successfull generated Do you want to "
+                "Receive Price Updates on this email?(y/n)"), 'yellow')
+        res = input()
+        if (res in ("Y", "y")):
+            email = input("Enter your Email")
+            CONF['receiver_email'].append(email)
+            data = CONF
+            with open("user_config.json", "w") as f:
+                json.dump(data, f, indent=4)
+                cprint("We added your email successfully", 'green')
         else:
-            cprint("Thanks for using us",'magenta')
-    def get_smartprix_code(self,mob_name):
-        print(mob_name)
-        q=requests.get(f"https://www.smartprix.com/products/?q={mob_name}")
-        s=bs4.BeautifulSoup(q.text,'lxml')
-        sel=s.select(".f-mobiles:nth-child(1) h2 a")
-        st=sel[0]['href']
-        x=st[st.find("mobiles/")+8:]
-        cprint(f"Smartprix Code -> {x}",'magenta')
-    def smart_compare(self,mob1,mob2):
-        q1=requests.get(f"https://www.smartprix.com/products/?q={mob1}")
-        q2=requests.get(f"https://www.smartprix.com/products/?q={mob2}")
-        s1=bs4.BeautifulSoup(q1.text,'lxml')
-        sel1=s1.select(".f-mobiles:nth-child(1) .score-val")[0]
-        s2=bs4.BeautifulSoup(q2.text,'lxml')
-        sel2=s2.select(".f-mobiles:nth-child(1) .score-val")[0]
-        #print(sel2,sel1)
-        if(int(sel2.text)>int(sel1.text)):
-            mob1,mob2=mob2,mob1
-        cprint(f"{mob1} is better than {mob2} comparing all the features",'cyan')
+            cprint("Thanks for using us", 'magenta')
+
+    def get_smartprix_code(self, mob_name):
+        # print(mob_name)
+        req = requests.get(f"https://www.smartprix.com/products/?q={mob_name}")
+        soup = bs4.BeautifulSoup(req.text, 'lxml')
+        sel = soup.select(".f-mobiles:nth-child(1) h2 a")
+        st = sel[0]['href']
+        x = st[st.find("mobiles/") + 8:]
+        cprint(f"Smartprix Code -> {x}", 'magenta')
+
+    def smart_compare(self, mob1, mob2):
+        req1 = requests.get(f"https://www.smartprix.com/products/?q={mob1}")
+        req2 = requests.get(f"https://www.smartprix.com/products/?q={mob2}")
+        soup1 = bs4.BeautifulSoup(req1.text, 'lxml')
+        sel1 = soup1.select(".f-mobiles:nth-child(1) .score-val")[0]
+        soup2 = bs4.BeautifulSoup(req2.text, 'lxml')
+        sel2 = soup2.select(".f-mobiles:nth-child(1) .score-val")[0]
+        # print(sel2,sel1)
+        if (int(sel2.text) > int(sel1.text)):
+            mob1, mob2 = mob2, mob1
+        cprint(f"{mob1} is better than {mob2}", 'cyan')
+
 
 if __name__ == "__main__":
-    if(len(sys.argv)>1):
-        if(sys.argv[1]=='-j'):
+    if (len(sys.argv) > 1):
+        if (sys.argv[1] == '-j'):
             Api().organize()
-        elif(sys.argv[1]=='-gc'):
-            if(len(sys.argv)!=3):
-                cprint("Enter a valid mobile name enter -h for help",'cyan')
+        elif (sys.argv[1] == '-gc'):
+            if (len(sys.argv) != 3):
+                cprint("Enter a valid mobile name enter -h for help", 'cyan')
             else:
                 Api().get_smartprix_code(sys.argv[2])
-        elif(sys.argv[1]=='-cm'):
-            if(len(sys.argv)!=4):
-                cprint("Enter a valid mobile name enter -h for help",'cyan')
+        elif(sys.argv[1] == '-cm'):
+            if (len(sys.argv) != 4):
+                cprint("Enter a valid mobile name enter -h for help", 'cyan')
             else:
-                Api().smart_compare(sys.argv[2],sys.argv[3])
-        elif(sys.argv[1]=='-h'):
-            cprint("-j -> Gets you a json file of Mobiles\n"+"-gc [Mobile Name]-> Gets you Smartprix Code for a particular Mobile\n"+"-cm [Mobile 1] [Mobie 2] to compare phones on basis of their spec score\n"+"-h -> Help Menu\n",'cyan')
+                Api().smart_compare(sys.argv[2], sys.argv[3])
+        elif (sys.argv[1] == '-h'):
+            HELPMENU = ('-j -> GetsyouajsonfileofMobiles\n'
+                        '-gc [Mobile Name]-> Gets you Smartprix Code'
+                        'for a Mobile\n'
+                        '-cm [Mobile 1] [Mobie 2] to compare phones on basis '
+                        'of their spec score\n'
+                        '-h -> Help Menu\n')
+            cprint(HELPMENU, 'cyan')
         else:
-            cprint("Enter a valid mobile name enter -h for help",'cyan')
+            cprint("Enter a valid mobile name enter -h for help", 'cyan')
     else:
-        cprint("Invalid Command -h for help",'red')
+        cprint("Invalid Command -h for help", 'red')
--- Week-1/aryan29/keeptrack.py
+++ Week-1/aryan29/keeptrack.py
@@ -1,64 +1,83 @@
 import time
+import sys
 import json
-import requests
 import smtplib
+import requests
 from termcolor import cprint
 from api import Api
-with open("user_config.json","r") as f:
-    conf=json.load(f)
-    if(conf["sender_email"]=="" or conf["sender_password"]=="" or len(conf["receiver_email"])==0):
-        cprint("Please Make sure to Enter Sender_Email, Receiver_Email and Sender_Password for Gmail before continuing",'red')
+with open("user_config.json", "r") as f:
+    CONF = json.load(f)
+    if (CONF["sender_email"] == "" or CONF["sender_password"] == "" or
+            len(CONF["receiver_email"]) == 0):
+        cprint(("Please Make sure to Enter Sender_Email, Receiver_Email"
+                "and Sender_Password for Gmail before continuing"), 'red')
         sys.exit()
 
+
 def keeptrack():
-    with open("data.json","r") as f:
-        obj=json.load(f)
-    #print(obj)
+    with open("data.json", "r") as f:
+        obj = json.load(f)
+    # print(obj)
     for mobile in obj:
-        name=mobile['name']
-        oldPrice=mobile['Best Price']
-        newPrice=get_current_price(mobile['Code'])
-        print(oldPrice,newPrice)
-        if(newPrice<oldPrice):
-            message=f"Best Price of {name} has changed from {oldPrice} to {newPrice} at {time.ctime(time.time())}"
-            sendEmail(message)
+        name = mobile['name']
+        oldPrice = mobile['Best Price']
+        newPrice = get_current_price(mobile['Code'])
+        print(oldPrice, newPrice)
+        if (newPrice < oldPrice):
+            message = (f"Best Price of {name} has changed from{oldPrice}"
+                       f"to {newPrice} at {time.ctime(time.time())}")
+            send_email(message)
             telegram_alert(message)
-            with open("logs.txt","w") as f:
-                print(message,file=f)
+            with open("logs.txt", "w") as f:
+                print(message, file=f)
+
+
 def get_current_price(mob):
-    x=Api().get_data(mob)
-    return Api().name_data(x,mob)['Best Price']
-def sendEmail(msg):
+    x = Api().get_data(mob)
+    return Api().name_data(x, mob)['Best Price']
+
+
+def send_email(msg):
     try:
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
-        s.login(conf["sender_email"], conf["sender_password"])
-        list=conf["receiver_email"]
+        s.login(CONF["sender_email"], CONF["sender_password"])
+        myList = CONF["receiver_email"]
         message = "Subject:Price Updates\n\n"+msg
-        print(f"Message is {message}")
-        for lines in list:
-            s.sendmail(conf['sender_email'], lines, message)
+        print(f"message is {message}")
+        for lines in myList:
+            s.sendmail(CONF['sender_email'], lines, message)
         s.quit()
-    except:
-        cprint("Email Credentials not avilable or they are not valid please allow less secure apps from your gmail if your credentials are correct",'red')
-def telegram_alert(bot_message):
+    except Exception as ex:
+        cprint(("Email Credentials not avilable or they are not valid "
+                "pleaseallow less secure apps from your "
+                " gmail if your credentials are correct"), 'red')
+        sys.exit()
+
+
+def telegram_alert(BotMessage):
     try:
-        bot_token=conf['telegram_bot_token']
-        bot_chatID=conf['telegram_chatID']
-        url="https://api.telegram.org/bot"+bot_token+'/sendMessage?chat_id'+bot_chatID+'&parse_mode=Markdown&text='+bot_message
-    except:
-        cprint("Telegram Credentials not avilable or they are not valid",'red')
-    res=requests.get(url)
+        botToken = CONF['telegram_bot_token']
+        botChatId = CONF['telegram_chatID']
+        url = ('https://api.telegram.org/bot' + botToken + '/sendMessage'
+               '?chat_id'+botChatId+'&parse_mode=Markdown&text='+BotMessage)
+    except Exception as ex:
+        cprint(f"While logging in Telegram {ex} occured", 'red')
+    requests.get(url)
+
+
 def checkserver():
-    msg="Daily Server Check"
-    #telegram_alert(msg)
-    sendEmail(msg)
+    msg = "Daily Server Check"
+    # telegram_alert(msg)
+    send_email(msg)
+
+
 if __name__ == "__main__":
-    updation_time=60*60*3 #3 hour updates
-    c=0
+    updationTime = 60 * 60 * 3  # 3 hour updates
+    COUNT = 0
     while(1):
-        c=c+1
+        COUNT = COUNT + 1
         keeptrack()
-        if(c%8==1):
-            checkserver()    #1day updates
-        time.sleep(updation_time)
+        if (COUNT % 8 == 1):
+            checkserver()    # 1day updates
+        time.sleep(updationTime)
